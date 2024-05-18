0648ad21178c15ba90e8699cb3cbf87e7c320d29
João Monteiro <94985251+jvftw@users.noreply.github.com>
2024-05-18 18:20:19 -0400

    logo do lanterna verde reativo

--- green_lantern/App.tsx
+++ green_lantern/App.tsx
@@ -1,19 +1,36 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
-import symbolOn from './assets/pictures/symbol-on.png'
+import { StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
 
 export default function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleSymbol() {
+    setIsActive((oldValue:boolean) => {return !oldValue})
+  }
+
   return (
-    <View style={styles.container}>
-        <Image source={symbolOn}></Image>
+    <View style={isActive ? styles.containerOn : styles.containerOff}>
+      <TouchableOpacity onPress={handleSymbol}>
+        <Image source={isActive ? symbolOn : symbolOff} />
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerOn: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  containerOff: {
+    flex: 1,
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
